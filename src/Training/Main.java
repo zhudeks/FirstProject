0b9496d986ba9618bb47d5016a49a6ad362a902e
zhudeks <ghazaryan.zhudeks@gmail.com>
2023-04-20 11:19:28 +0400

    //
    Signed-off-by: zhudeks <ghazaryan.zhudeks@gmail.com>

--- src/Training/Main.java
+++ src/Training/Main.java
@@ -2,32 +2,7 @@ package Training;
 
 public class Main {
 	public static void main(String[] args) {
-		/*
-		Read input from keyboard
-		Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-		System.out.print("Enter age: ");
-
-		int num = myObj.nextInt();  // Read user input
-		System.out.println("Factorial of " + num + " is " + fact(num));  // Output user input
-		*/
-
-		int[] input_array = {3, 3, 1, 1, 1, 1, 2};
-		int[] input_array1 = {3, 1};
-		int[] input_array2 = {12, 5, 89, 100, 1022, 101, -1, 1, 0, 2, -5, 0, 9, 2, -1, 65, -100, -5, -1, 121, 3, -2};
-		int[] input_array3 = {-4, -5};
-		int[] input_array4 = {3, -2, -1, 4, 5, 2, 0, 0, 2, 1, 1, 2, 9, 0, 7, 2, -36, 4};
-		double[] input_array5 = {-2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		int[] input_array6 = {-3, -2, 17, 2, 5, 36, 4, 5, 0};
-		char[] input_array7 = {'l','+', 'o', ';', 'K', '7', 'a', 'f', '/', 'a', 'b', ' ', '?', 'b', 'b'};
-		double[] input_array8 = {-0.5, -0.21, -0.7, -0.2, -16, 0.4, 0.5, 0};
-		int[][] input_array9 = {{-104, 3, -3, 1},
-								{0, 1, 3, 0},
-								{8, -5, 12, 100},
-								{3, 100, -9, 100}};
-
-		Tasks.task028(input_array9);
-		//First commit on dev
-
+		Box newBox = new Box();
 
 	}
 }

4386e25257a450857d4820ea7bf60a103476aa7c
zhudeks <ghazaryan.zhudeks@gmail.com>
2023-04-21 19:08:18 +0400

    //
    Signed-off-by: zhudeks <ghazaryan.zhudeks@gmail.com>

--- src/Training/Main.java
+++ src/Training/Main.java
@@ -2,11 +2,6 @@ package Training;
 
 public class Main {
 	public static void main(String[] args) {
-		//comment for test env
-
-		//another comment for test env
-
-		//dsfjnsdfjksdnfkjs
-		//sdfdsfsd
+		//comment for future branch
 	}
 }
